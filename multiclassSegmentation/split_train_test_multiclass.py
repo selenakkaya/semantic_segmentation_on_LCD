8f702d7b43b101aa93f1a01ff3ebc5c8d1fdbd08
Selen Akkaya <50169967+selenakkaya@users.noreply.github.com>
2022-11-18 10:50:35 +0100

    Add files via upload

---
+++ multiclassSegmentation/split_train_test_multiclass.py
@@ -0,0 +1,102 @@
+"""
+Split the dataset in train, validation and test for multiclass model.
+The splitting is equally distributed between sites.
+Square images cropped from one image go to the same set.
+Save numpy array for the three splits for images and masks.
+Run for mesh or wire
+"""
+
+import random
+import numpy as np
+
+random.seed(1)
+
+SIZE = 512
+TRAIN_SIZE = 0.75
+VAL_SIZE = 0.15
+
+src_path = "saved_arrays_multiclass"
+images = np.load(src_path + "/images.npy")
+masks = np.load(src_path + "/one_hot_masks.npy")
+filenames = np.load(src_path + "/filenames.npy")
+
+print(filenames)
+
+site1 = []
+site2 = []
+site3 = []
+for filename in filenames:
+    fileN= int(filename.split('_')[1][3:])
+
+    if(fileN < 173 and fileN not in site1):
+        site1.append(fileN)
+    elif(fileN >= 173 and fileN < 286 and fileN not in site2 ):
+        site2.append(fileN)
+    elif (fileN >= 286 and fileN not in site3):
+        site3.append(fileN)
+
+
+n_train_1 = int(len(site1)*TRAIN_SIZE)
+n_train_2 = int(len(site2)*TRAIN_SIZE)
+n_train_3 = int(len(site3)*TRAIN_SIZE)
+
+n_val_1 = int(len(site1)*VAL_SIZE)
+n_val_2 = int(len(site2)*VAL_SIZE)
+n_val_3 = int(len(site3)*VAL_SIZE)
+
+random.shuffle(site1)
+random.shuffle(site2)
+random.shuffle(site3)
+
+train_list_1 = site1[:n_train_1]
+train_list_2 = site2[:n_train_2]
+train_list_3 = site3[:n_train_3]
+
+val_list_1 = site1[n_train_1:(n_train_1+n_val_1)]
+val_list_2 = site2[n_train_2:(n_train_2+n_val_2)]
+val_list_3 = site3[n_train_3:(n_train_3+n_val_3)]
+
+test_list_1 = site1[(n_train_1+n_val_1):]
+test_list_2 = site2[(n_train_2+n_val_2):]
+test_list_3 = site3[(n_train_3+n_val_3):]
+
+
+train_images = []
+val_images = []
+test_images = []
+train_masks = []
+val_masks = []
+test_masks = []
+for i in range(filenames.shape[0]):
+    fileN = int(filenames[i].split('_')[1][3:])
+
+    if(fileN in train_list_1 or fileN in train_list_2 or fileN in train_list_3):
+        train_images.append(images[i])
+        train_masks.append(masks[i])
+    elif(fileN in val_list_1 or fileN in val_list_2 or fileN in val_list_3):
+        val_images.append(images[i])
+        val_masks.append(masks[i])
+    elif(fileN in test_list_1 or fileN in test_list_2 or fileN in test_list_3):
+        test_images.append(images[i])
+        test_masks.append(masks[i])
+
+train_images = np.array(train_images)
+val_images = np.array(val_images)
+test_images = np.array(test_images)
+train_masks = np.array(train_masks)
+val_masks = np.array(val_masks)
+test_masks = np.array(test_masks)
+
+print("train_images", train_images.shape)
+print("val_images", val_images.shape)
+print("test_images", test_images.shape)
+print("train_mask", train_masks.shape)
+print("val_mask", val_masks.shape)
+print("test_mask", test_masks.shape)
+
+np.save(src_path + "/train_images.npy", train_images)
+np.save(src_path + "/val_images.npy", val_images)
+np.save(src_path + "/test_images.npy", test_images)
+np.save(src_path + "/train_masks.npy", train_masks)
+np.save(src_path + "/val_masks.npy", val_masks)
+np.save(src_path + "/test_masks.npy", test_masks)
---
+++ multiclassSegmentation/unet_multiclass.py
@@ -0,0 +1,154 @@
+import numpy as np
+import tensorflow as tf
+import keras
+from keras.models import Model
+from keras.layers import Conv2D, MaxPooling2D, Input, Conv2DTranspose, Concatenate
+import random
+from keras.callbacks import ModelCheckpoint
+from keras import backend as K
+import cv2
+import os
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+
+
+
+DIM = 512
+EPOCHS = 250
+PATIENCE = 50
+BATCH_SIZE = 32
+OPTIMIZER = 'adam'
+DIM = 512
+MODEL_NAME = "unet_multiclass.h5"
+
+src_path = "../processing/saved_arrays_multiclass"
+train_images = np.load(src_path + "/train_images.npy")[:100] #FIXME aggiungere tutte le immagini
+train_masks = np.load(src_path + "/train_masks.npy")[:100]
+val_images = np.load(src_path + "/val_images.npy")[:100]
+val_masks = np.load(src_path + "/val_masks.npy")[:100]
+test_images = np.load(src_path + "/test_images.npy")[:100]
+test_masks = np.load(src_path + "/test_masks.npy")[:100]
+print("train images", train_images.shape)
+print("train masks", train_masks.shape)
+print("val images", val_images.shape)
+print("test images", test_images.shape)
+
+dst_dir = "samples_multiclass/during_training"
+if not os.path.exists(dst_dir):
+    os.makedirs(dst_dir)
+
+train_images = train_images/255.0
+val_images = val_images/255.0
+test_images = test_images/255.0
+
+checkpoint_path = "checkpoints" #where to save the model checkpoints
+if not os.path.exists(checkpoint_path):
+    os.mkdir(checkpoint_path)
+
+
+def mean_iou(y_true, y_pred):
+    yt0 = y_true[:,:,:,0]
+    yp0 = K.cast(y_pred[:,:,:,0] > 0.5, 'float32')
+    inter = tf.math.count_nonzero(tf.logical_and(tf.equal(yt0, 1), tf.equal(yp0, 1)))
+    union = tf.math.count_nonzero(tf.add(yt0, yp0))
+    iou = tf.where(tf.equal(union, 0), 1., tf.cast(inter/union, 'float32'))
+    return iou
+
+
+def unet(sz, n_classes=3):
+    x = Input(sz)
+    inputs = x
+    # down sampling
+    f = 8
+    layers = []
+
+    for i in range(0, 6):
+        x = Conv2D(f, 3, activation='relu', padding='same')(x)
+        x = Conv2D(f, 3, activation='relu', padding='same')(x)
+        layers.append(x)
+        x = MaxPooling2D()(x)
+        f = f * 2
+    ff2 = 64
+
+    # bottleneck
+    j = len(layers) - 1
+    x = Conv2D(f, 3, activation='relu', padding='same')(x)
+    x = Conv2D(f, 3, activation='relu', padding='same')(x)
+    x = Conv2DTranspose(ff2, 2, strides=(2, 2), padding='same')(x)
+    x = Concatenate(axis=3)([x, layers[j]])
+    j = j - 1
+
+    # upsampling
+    for i in range(0, 5):
+        ff2 = ff2 // 2
+        f = f // 2
+        x = Conv2D(f, 3, activation='relu', padding='same')(x)
+        x = Conv2D(f, 3, activation='relu', padding='same')(x)
+        x = Conv2DTranspose(ff2, 2, strides=(2, 2), padding='same')(x)
+        x = Concatenate(axis=3)([x, layers[j]])
+        j = j - 1
+
+    # classification
+    x = Conv2D(f, 3, activation='relu', padding='same')(x)
+    x = Conv2D(f, 3, activation='relu', padding='same')(x)
+    outputs = Conv2D(n_classes, 1, activation='softmax')(x)
+
+    # model creation
+    model = Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy', mean_iou])
+    return model
+
+
+def plot_img_and_masks(raw, mask):
+    pred = model.predict(np.expand_dims(raw, 0))
+    pred_msk = pred.squeeze()
+    pred_msk = np.stack((pred_msk,) * 3, axis=-1)
+    pred_msk[pred_msk >= 0.5] = 1
+    pred_msk[pred_msk < 0.5] = 0
+    target_msk = np.stack((mask,) * 3, axis=-1)
+    raw = np.float32(raw)
+    raw = cv2.cvtColor(raw, cv2.COLOR_RGB2BGR)
+    combined = np.concatenate([raw, pred_msk, target_msk], axis=1)
+    return combined
+
+
+def build_callbacks():
+    checkpointer = [
+          EarlyStopping(monitor="val_mean_iou", patience=PATIENCE, restore_best_weights=True, mode="max"),
+          ModelCheckpoint(checkpoint_path + '/'+ MODEL_NAME,  monitor="val_mean_iou", save_best_only=True, mode='max') #best on iou
+    ]
+    return checkpointer
+
+
+model = unet(sz=(DIM,DIM,3))
+print(model.summary())
+
+model.fit(train_images, train_masks,  batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(val_images, val_masks), callbacks = build_callbacks())
+
+
+sample_dir = "samples_multiclass" #where to save the predictions
+if not os.path.exists(sample_dir):
+    os.mkdir(sample_dir)
+
+for i in range(test_images.shape[0]):
+    test_img = test_images[i]
+    test_mask = test_masks[i][:,:,0]
+    combined = plot_img_and_masks(test_img, test_mask)
+    cv2.imwrite("samples/" + str(i) + '.jpg', combined * 255)
+
+
+"""Quantitative measures"""
+print("TEST RESULTS")
+pred_masks = model.predict(test_images)
+
+iou = mean_iou_test(test_masks, pred_masks)
+print("mean iou", iou)
+dice = dice_coeff(test_masks, pred_masks)
+print("dice coeff.", dice)
+acc = pixel_accuracy(test_masks, pred_masks)
+print("pixel acc.", acc)
+
+
+
+
+
+
