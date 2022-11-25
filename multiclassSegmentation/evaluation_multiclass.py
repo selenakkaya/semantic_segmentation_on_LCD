4e49884992638160632dfa7a821f0bfe41ef7aad
Selen Akkaya <50169967+selenakkaya@users.noreply.github.com>
2022-11-25 16:28:23 +0100

    Update evaluation_multiclass.py

--- multiclassSegmentation/evaluation_multiclass.py
+++ multiclassSegmentation/evaluation_multiclass.py
@@ -57,7 +57,7 @@ def dice_coeff(y_true, y_pred, num_classes):
 
 
 
-def pixel_accuracy(y_true, y_pred, num_classes):
+def pixel_accuracy_one_class(y_true, y_pred, num_classes):
     correct_pred = np.zeros(num_classes) # int = (mesh : M, wire : W and background : B)
 
     for i in range (y_true.shape[0]):
@@ -78,3 +78,45 @@ def pixel_accuracy(y_true, y_pred, num_classes):
     pix_acc=pix_acc/(num_classes-1)
     return pix_acc
 
+
+
+def pixel_accuracy_backup(y_true, y_pred, num_classes):
+    correct_pred = np.zeros(num_classes) # int = (mesh : M, wire : W and background : B)
+
+    for i in range (y_true.shape[0]):
+        for j in range(y_true.shape[1]): # height
+            for k in range(y_true.shape[2]): # width
+                if y_pred[i][j][k]==y_true[i][j][k]:
+                    correct_pred[y_true[i][j][k]]+=1
+    
+
+    pix_acc = 0
+    for i in range(num_classes):
+        tot = (y_true==i).sum()
+        if tot!=0:
+            pix_acc+=correct_pred[i]/tot
+        else:
+            pix_acc+=pix_acc
+
+    pix_acc=pix_acc/num_classes
+    return pix_acc
+
+
+
+def pixel_accuracy(y_true, y_pred, num_classes):
+    correct_pred = np.zeros(num_classes) # int = (mesh : M, wire : W and background : B)
+
+    for i in range (y_true.shape[0]):
+        for j in range(y_true.shape[1]): # height
+            for k in range(y_true.shape[2]): # width
+                if y_pred[i][j][k]==y_true[i][j][k]:
+                    correct_pred[y_true[i][j][k]]+=1
+    
+    pix_acc_classes = []
+    for i in range(num_classes):
+        tot = (y_true==i).sum()
+        if tot!=0:
+            pix_acc_classes.append(correct_pred[i]/tot)
+    
+    mean_pixel_acc = sum(pix_acc_classes)/len(pix_acc_classes)
+    return mean_pixel_acc, pix_acc_classes
