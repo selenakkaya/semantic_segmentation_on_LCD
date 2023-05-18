8325b6c0808f7bd0d4e0380711ecf68bfdec8ce6
Selen Akkaya <50169967+selenakkaya@users.noreply.github.com>
2023-05-18 11:03:04 +0200

    Add files via upload

--- std_unet/wire/std_unet_wire/evaluation.py
+++ std_unet/wire/std_unet_wire/evaluation.py
@@ -1,1 +1,41 @@
+"""
+Functions to evaluate semantic segmentation models
+"""
+
 import numpy as np
+
+
+
+# intersection over : percent overlap between the target mask and our prediction
+def mean_iou_test(y_true, y_pred):
+    yt0 = y_true[:,:,:,0]
+    yp0 = (y_pred[:,:,:,0] > 0.5).astype(int)
+    intersection = np.logical_and(yt0, yp0)
+    union = np.logical_or(yt0, yp0)
+    iou_score = np.sum(intersection) / np.sum(union)
+    return iou_score
+
+# dice coefficient
+def dice_coeff(y_true, y_pred):
+    yt0 = y_true[:,:,:,0]
+    yp0 = (y_pred[:,:,:,0] > 0.5).astype(int)
+    intersection = np.logical_and(yt0, yp0)
+    union = np.logical_or(yt0, yp0)
+    dice_coeff = 2*np.sum(intersection)/(np.sum(union) + np.sum(intersection))
+    return dice_coeff
+
+def pixel_accuracy(y_true, y_pred):
+    t0 = y_true[:,:,:,0]
+    yp0 = (y_pred[:,:,:,0] > 0.5).astype(int)
+    correct_preds = np.sum(np.equal(t0,yp0))
+    tot = y_true.shape[0]*y_true.shape[1]*y_true.shape[2]
+    acc = correct_preds/tot
+    return acc
+
+def pixel_accuracy_one_class(y_true, y_pred):
+    yt0 = y_true[:,:,:,0]
+    yp0 = (y_pred[:,:,:,0] > 0.5).astype(int)
+    intersection = np.logical_and(yt0, yp0) #TP
+    class1 = np.sum(yt0) #TP+FN
+    acc = np.sum(intersection)/class1
+    return acc
